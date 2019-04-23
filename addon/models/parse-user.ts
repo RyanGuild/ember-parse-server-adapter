bc076f4edd7e8144b92466fff2036fcb713acc5f
Ryan Guild <ryanwork489@gmail.com>
2019-04-22 22:38:09 -0400

    promisify login api

--- addon/models/parse-user.ts
+++ addon/models/parse-user.ts
@@ -1,14 +1,6 @@
 import DS from 'ember-data'
-import RSVP, {
-  reject
-} from 'rsvp'
-import {
-  isEmpty
-} from '@ember/utils'
-import Ember from 'ember'
-import Mixin from '@ember/object/mixin';
+import RSVP from 'rsvp'
 import {debug} from '@ember/debug'
-import {typeOf} from '@ember/utils'
 
 
 var ParseUser = DS.Model.extend(
@@ -24,80 +16,64 @@ var ParseUser = DS.Model.extend(
 );
 
 ParseUser.reopenClass({
-  requestPasswordReset: function (email) {
-    var adapter = this.get('store').adapterFor('parse-user'),
-      data = {
-        email: email
-      };
 
-    return adapter.ajax(adapter.buildURL('requestPasswordReset'), 'POST', {
-      data: data
-    })['catch'](
-      function (response) {
-        return reject(response.responseJSON);
-      }
-    );
+
+  requestPasswordReset: function (email) {
+    let adapter = this.get('store').adapterFor('parse-user')
+    let data = {email: email};
+
+    return new RSVP.Promise((resolve, reject) => {
+      adapter.ajax(adapter.buildURL('requestPasswordReset'), 'POST', {
+        data: data
+      })
+      .catch((response) => reject(response.responseJSON))
+    })
   },
+
 
   login: function (store, data) {
     let model = this
     let adapter = store.adapterFor('parse-user')
     let serializer = store.serializerFor('parse-user')
 
-    if (isEmpty(this.typeKey)) {
-      throw new Error('Parse login must be called on a model fetched via store.modelFor');
-    }
-
-    return adapter.ajax(adapter.buildURL('login'), 'GET', {
-      data: data
-    }).then(
-      function (response) {
+    return new RSVP.Promise((resolve, reject) => {
+      adapter.ajax(adapter.buildURL('login'), 'GET', {
+        data: data
+      }).then((response) => {
         serializer.normalize(model, response);
         var record = store.push(model, response);
-        return record;
-      },
-      function (response) {
-        return reject(response.responseJSON);
-      }
-    );
+        resolve(record)
+      })
+      .catch((response) => reject(response.responseJSON))
+    })
   },
+
 
   signup: function (store :DS.Store,data: {username: string,email: string,password: string}): RSVP.Promise<DS.Model | any[]> 
     {
       let model :DS.Model = this
       //@ts-ignore
       let adapter :DS.RESTAdapter = store.adapterFor('parse-user')
-      
       //@ts-ignore
       let serializer :DS.RESTSerializer = store.serializerFor('parse-user')
 
-      debug(adapter.toString()+" and "+ serializer.toString())
-
-      return new RSVP.Promise((resolve, _) => { 
-      let newUserUrl = adapter.buildURL('parse-user')
-      console.log('new user url:', newUserUrl)
-      //@ts-ignore
-      adapter.ajax(newUserUrl, 'POST', {
-        data: data
-      })
-      .then(
-        function (response) {
-          console.log('new user json response:', JSON.stringify(response))
-          let merged = Object.assign({}, data, response)
-          console.log('new user:', JSON.stringify(merged))
-          let normalized = store.normalize('parse-user', merged)
-          console.log('normalized:', JSON.stringify(normalized))
-          //@ts-ignore
-          let record = store.push(normalized)
-          resolve(record)
+      return new RSVP.Promise((resolve, reject) => { 
+        let newUserUrl = adapter.buildURL('parse-user')
+        console.log('new user url:', newUserUrl)
+        //@ts-ignore
+        adapter.ajax(newUserUrl, 'POST', {
+          data: data
         })
-      .catch(
-        function(e :ExceptionInformation){
-          throw `failed to create user: ${e}`
-        })
+        .then((response) => {
+            let merged = Object.assign({}, data, response)
+            let normalized = store.normalize('parse-user', merged)
+            //@ts-ignore
+            let record = store.push(normalized)
+            resolve(record)
+          })
+        .catch((e) => reject(e))
       })
     }
-
 })
 
 
